86b2f58ac2bdc2f722d547e3b90a0b91380eca1a
lilitantonyan <lilit@helix.am>
2024-10-26 14:54:39 +0400

    add files

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function (){
+    var text_max = 100;
+    $('#count_message').html('0 / ' + text_max );
+
+    $('#text').keyup(function() {
+        var text_length = $('#text').val().length;
+        var text_remaining = text_max - text_length;
+
+        $('#count_message').html(text_length + ' / ' + text_max);
+    });
+})
